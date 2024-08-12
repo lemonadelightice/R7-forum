a2197338332b23be924983234366d1ebe412722f
lemonade <nicole.jordanncj@gmail.com>
2024-08-12 13:49:28 -0400

    added user subscription functionality

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  
   get 'posts/create'
   get 'posts/new'
   get 'posts/edit'
@@ -17,8 +18,8 @@ Rails.application.routes.draw do
   patch '/users/:id', to: 'users#update'
   delete '/users/:id', to: 'users#delete'
   resources :forums do 
-    resources :posts, shallow: true, except: [:index]
-    resources :subscriptions, shallow: true, except: [:index]
+  resources :posts, shallow: true, except: [:index]
+  resources :subscriptions, shallow: true, except: [:index]
   end
   get '/subscriptions', to: 'subscriptions#index', as: 'subscriptions'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
